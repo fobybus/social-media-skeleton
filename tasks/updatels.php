469afc0d465e948bc64eff222d830ae7522b215d
mblunt <52963601+mblunt@users.noreply.github.com>
2023-08-03 12:29:15 -0400

    SQLi fix

--- tasks/updatels.php
+++ tasks/updatels.php
@@ -1,41 +1,41 @@
 <?php 
 require('condb.php');
-$postede=$_GET['email'];
-//get id from the url sent to this page 
-//updatels.php?email=test@gmail.com
-$search="select * from users where email='$postede'";
-$result=$dbcon->query($search);
-//for debugging only not for users message 
-if($result)
-{
-  //  echo("successfully parsed id");
-   
-}  else {
- /// echo("can't parse id");
-  ///echo mysqli_error($dbcon);
-  
+
+$postedemail = $_GET['email'];
+
+// Get id from the url sent to this page 
+$search = "SELECT * FROM users WHERE email=?";
+$stmt = mysqli_prepare($dbcon, $search);
+mysqli_stmt_bind_param($stmt, "s", $postedemail);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+// For debugging only not for users message 
+if ($result) {
+    //  echo("successfully parsed id");
+} else {
+    // echo("can't parse id");
+    // echo mysqli_error($dbcon);
 }
-//find id 
-$row=$result->fetch_assoc();
-$uid=$row['id'];
-//update user last seen when ever user moves nevigate from one page to another 
-//get id from the url sent to this page 
-//updatels.php?uid=10000
-$ls= date('Y/m/d/H/i/s');
-$search="update users set last_seen='$ls' where id=$uid";
-$result=$dbcon->query($search);
-//for debugging only not for users message 
-if($result)
-{
-   // echo("successfully updated");
-   
-}  else {
- // echo("can't update ");
- // echo mysqli_error($dbcon);
-  
+
+// Find id 
+$row = $result->fetch_assoc();
+$uid = $row['id'];
+
+// Update user last seen whenever user navigates from one page to another 
+$ls = date('Y/m/d/H/i/s');
+$update_query = "UPDATE users SET last_seen=? WHERE id=?";
+$stmt = mysqli_prepare($dbcon, $update_query);
+mysqli_stmt_bind_param($stmt, "si", $ls, $uid);
+mysqli_stmt_execute($stmt);
+
+// For debugging only not for users message 
+if ($stmt->affected_rows > 0) {
+    // echo("successfully updated");
+} else {
+    // echo("can't update ");
+    // echo mysqli_error($dbcon);
 }
 
 $dbcon->close();
-
-
 ?>
