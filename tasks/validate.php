c1e0e7584352ca095625287d4c2e9d7001c583ae
fobybus <62669476+fobybus@users.noreply.github.com>
2023-08-17 04:23:10 -0700

    added backend validation php file

---
+++ tasks/validate.php
@@ -0,0 +1,52 @@
+<?php
+//validator php  foby bus @msf black eagles 
+//created 2023/08
+//last update=2023/08  
+function validateEmail($email)
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function validateText($text)
+{
+    return preg_match("/^[A-Za-z]+$/", $text);
+}
+
+function validateDate($date)
+{
+    echo boolval(strtotime($date));
+}
+
+function validateGender($gender)
+{
+    if ($gender == "male" || $gender == "female")
+        return true;
+    else
+        return false;
+}
+
+//minimum 8 characters 
+function validatePass($pass)
+{
+    return preg_match("/^.{8,}$/", $pass);
+}
+
+
+//comntinu letter 
+function validateSignup($email, $pass, $fname, $lname, $gender, $date, $city)
+{
+    if (validateEmail($email) && validatePass($pass) && validateText($fname) && validateText($lname) && validateGender($gender) && validateText($city) && validateDate($date))
+        return true;
+    else
+        return false;
+}
+
+function validateUserInfo($email, $fname, $lname, $city)
+{
+    if (validateEmail($email) && validateText($fname) && validateText($lname) && validateText($city))
+        return true;
+    else
+        return false;
+}
+
+?>
